b21499868af5c4d0152cbf15d2bb2abe04e18137
minbo2002 <minbo2002@naver.com>
2023-10-14 05:47:29 +0900

    chore: 주석 설명 수정

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -55,9 +55,13 @@ class StockServiceTest {
     public void stockDecreaseTest2() throws InterruptedException {
 
         int threadCount = 100;  // 쓰레드 개수 100개
-        ExecutorService executorService = Executors.newFixedThreadPool(32);    // 멀티쓰레드를 사용하기 위해 ExecutorService 사용
-                                                                                       // ExecutorService : 동시로 실행하는 작업을 단순화하여 관리할 수 있도록 도와주는 유틸리티 클래스
-        CountDownLatch latch = new CountDownLatch(threadCount);// 100개의 요청이 모두 끝날때까지 기다리기위해
+
+        /* 멀티쓰레드를 사용하기 위해 ExecutorService 사용
+           ExecutorService : 동시로 실행하는 작업을 단순화하여 관리할 수 있도록 도와주는 유틸리티 클래스    */
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        /* CountDownLatch : 다른 쓰레드에서 수행중인 작업이 완료될때까지 대기할 수 있도록 도와주는 클래스   */
+        CountDownLatch latch = new CountDownLatch(threadCount);  // latch 숫자 100개
 
         for(int i=0; i<threadCount; i++) {
 
@@ -65,16 +69,15 @@ class StockServiceTest {
                 try {
                     stockService.decrease(1L, 1L);  // 상품의 수량 1개 감소
                 }finally {
-                    latch.countDown();  // latch.countDown() : 다른 쓰레드에서 수행중인 작업이 완료될때까지 대기할 수 있도록 도와주는 클래스
+                    latch.countDown();  // latch.countDown() : latch 숫자 1개씩 감소
                 }
             });
         }
 
-        latch.await();  // 모든 요청이 완료된다면
+        latch.await();  // latch.await() : latch의 숫자가 0이 될때까지 대기.  즉 모든 요청이 완료될때까지 대기
 
         Stock stock = stockRepository.findById(1L).orElseThrow();  // 100 -(1*100) = 0 예상
 
-        assertEquals(0, stock.getQuantity());
-
+        assertEquals(0, stock.getQuantity());  // 결과  --> 쓰레드 race condition에 의한 테스트 실패
     }
 }
